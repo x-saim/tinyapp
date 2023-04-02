65c575cd6880affac2b2bef080535f9fc1b3209c
Saim Amir <saimxamir@gmail.com>
2023-04-02 11:59:38 -0400

    add conditional for edge case in POST route for /urls/:id

--- express_server.js
+++ express_server.js
@@ -39,7 +39,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  ///edge case: if user inputs url without http/https protocol
+  //edge case: if user inputs url without http/https protocol
   let longURL = req.body["longURL"];
   if (!longURL.includes("http://") && !longURL.includes("https://")) {
     longURL = "https://" + longURL;
@@ -47,7 +47,7 @@ app.post("/urls", (req, res) => {
   const id = generateRandomString();
   urlDatabase[id] = longURL;
   res.redirect(`/urls/${id}`); // redirect the client to the /urls/:id route for the newly created short URL
-  }
+}
 );
 
 //separate urls for each short url id
@@ -55,7 +55,6 @@ app.get("/urls/:id", (req,res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show",templateVars);
 });
-
 
 //redirect short urls to long urls
 app.get("/u/:id", (req,res) => {
@@ -66,16 +65,19 @@ app.get("/u/:id", (req,res) => {
 //a POST route that updates a URL resource
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id; //assignign ID from URL /urls/<%= id %> to var id.
-  const newLongURL = req.body.longURL;
-  urlDatabase[id] = newLongURL;
-  console.log(urlDatabase);
+  let updatedLongURL = req.body.longURL;
+  
+  //edge case: if user inputs url without http/https protocol
+  if (!updatedLongURL.includes("http://") && !updatedLongURL.includes("https://")) {
+    updatedLongURL = "https://" + updatedLongURL;
+  }
+  urlDatabase[id] = updatedLongURL;
   res.redirect("/urls");
 });
 
-
 //Add a POST route that removes a URL resource.
 app.post("/urls/:id/delete",(req,res) => {
-  delete urlDatabase[req.params.id]
+  delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
@@ -84,11 +86,6 @@ app.get("/urls.json", (req,res) => {
   res.json(urlDatabase);
 });
 
-// sample for creating a new route and adding some context with HTML.
-app.get("/hello", (req,res) =>{
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
