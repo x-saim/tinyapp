1f197dabc81b4b222aeef64851d226f42531de9b
Saim Amir <saimxamir@gmail.com>
2023-04-08 11:55:20 -0400

    update function code and add more test cases

--- helpers.js
+++ helpers.js
@@ -1,18 +1,22 @@
 
-/*
-Function returns user object if inputted email matches existing
-*/
+/**
+ * Returns a user object from a database that matches a given email address, ignoring any leading or trailing white spaces, and case-insensitive.
+ * @param {string} email - The email address to search for in the database.
+ * @param {Object} database - The database object to search for the user with the given email address.
+ * @returns {Object|undefined} - The user object from the database that matches the given email address, or undefined if no such user exists in the database.
+ */
+
 const getUserByEmail = (email,database) => {
   for (const userId in database) {
     const user = database[userId];
-    if (user.email === email) {
+    if (user.email === email.toLowerCase().trim()) {
       return user;
     }
   }
 
-  return null;
+  return undefined;
 };
 
 module.exports = {
   getUserByEmail
-}
+};
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,21 +4,50 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function() {
+describe('getUserByEmail', () => {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.equal(user.id,expectedUserID);
   });
+
+  it(`should return undefined if non-existent email`, () => {
+    const user = getUserByEmail("invalid@email.com", testUsers);
+    assert.isUndefined(user);
+  });
+
+  it('should return undefined if the database is empty', function() {
+    const emptyDatabase = {};
+    const user = getUserByEmail("user@example.com", emptyDatabase);
+    assert.isUndefined(user);
+  });
+
+  it('should return a user with valid email that has uppercase characters', function() {
+    const user = getUserByEmail("USER@EXAMPLE.COM", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return a user with valid email that has leading and/or trailing spaces', function() {
+    const user = getUserByEmail("  user@example.com  ", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return undefined if the email is an empty string', function() {
+    const user = getUserByEmail("", testUsers);
+    assert.isUndefined(user);
+  });
+
 });
